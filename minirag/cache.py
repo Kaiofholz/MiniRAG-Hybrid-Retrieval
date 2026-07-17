911d2800a8de21a2010a065a46ed8e89fa08d447
holzofholz <kaizhang115@gmail.com>
2026-07-17 16:21:19 +0000

    Fix cache normalization test

--- minirag/cache.py
+++ minirag/cache.py
@@ -2,17 +2,23 @@ class AnswerCache:
     def __init__(self):
         self.cache = {}
 
+    def make_key(self, question):
+        return question.strip().lower()
+
     def get(self, question):
-        return self.cache.get(question)
+        key = self.make_key(question)
+        return self.cache.get(key)
 
     def save(self, question, result):
-        self.cache[question] = result
+        key = self.make_key(question)
+        self.cache[key] = result
 
     def clear(self):
         self.cache.clear()
 
     def __len__(self):
         return len(self.cache)
+
 
 class LLMCache:
     def __init__(self):
